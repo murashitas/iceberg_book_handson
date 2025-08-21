48c25c23ea9518dc8ff3fef00a07a652a402eb56
tomtongue <tanaka1201t@gmail.com>
2025-08-21 22:19:44 +0900

    Add catalog initialization and Iceberg metrics table creation

--- custom-metrics-reporter-iceberg/README.md
+++ custom-metrics-reporter-iceberg/README.md
@@ -15,3 +15,23 @@ $ ./gradlew jar
 ```
 
 3. After the build is complete, `custom-metrics-reporter-iceberg-<version>.jar` is created in `./build/libs`.
+
+## Configure Metrics Reporter for Spark
+
+```py
+spark = (
+    SparkSession.builder
+        .config("spark.jars.packages", "./custom-metrics-reporter-iceberg-1.0-SNAPSHOT.jar")
+        .config(f"spark.sql.catalog.{CATALOG}", "org.apache.iceberg.spark.SparkCatalog")
+        .config(f"spark.sql.catalog.{CATALOG}.type", "rest")
+        .config(f"spark.sql.catalog.{CATALOG}.uri", CATALOG_URL)
+        .config(f"spark.sql.catalog.{CATALOG}.s3.endpoint", S3_ENDPOINT)
+        .config(f"spark.sql.catalog.{CATALOG}.view-endpoints-supported", "true")
+        .config(f"spark.sql.catalog.{CATALOG}.metrics-reporter-impl", "jp.gihyo.iceberg.IcebergTableMetricsReporter")
+        .config(f"spark.sql.catalog.{CATALOG}.namespace", "db")
+        .config(f"spark.sql.catalog.{CATALOG}.table-prefix", "metrics_report")
+        .config("spark.sql.extensions", "org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions")
+        .config("spark.sql.defaultCatalog", "my_catalog")
+        .getOrCreate()
+)
+```
--- custom-metrics-reporter-iceberg/src/main/java/jp/gihyo/iceberg/IcebergTableMetricsReporter.java
+++ custom-metrics-reporter-iceberg/src/main/java/jp/gihyo/iceberg/IcebergTableMetricsReporter.java
@@ -1,46 +1,59 @@
 package jp.gihyo.iceberg;
 
+import org.apache.iceberg.CatalogUtil;
 import org.apache.iceberg.Schema;
+import org.apache.iceberg.Table;
 import org.apache.iceberg.catalog.Catalog;
 import org.apache.iceberg.catalog.TableIdentifier;
 import org.apache.iceberg.metrics.MetricsReport;
 import org.apache.iceberg.metrics.MetricsReporter;
-import org.apache.iceberg.rest.RESTCatalog;
 
 import java.util.Map;
 
 public class IcebergTableMetricsReporter implements MetricsReporter {
-    private static final IcebergTableMetricsReporter INSTANCE = new IcebergTableMetricsReporter();
+    private static final String CATALOG_NAME = "catalog-name";
+    private static final String CATALOG_IMPL = "catalog-impl";
+    private static final String NAMESPACE = "ns";
+    private static final String NAMESPACE_DEFAULT = "db";
+    private static final String TABLE_NAME_PREFIX = "table-prefix";
+    private static final String TABLE_NAME_PREFIX_DEFAULT = "report";
 
-    public static IcebergTableMetricsReporter getInstance() {
-        return INSTANCE;
-    }
+    private Catalog catalog;
+    private Map<String, String> properties;
+    private Table scanReportTable;
+    private Table commitReportTable;
 
     @Override
     public void initialize(Map<String, String> properties) {
-        Catalog catalog = new RESTCatalog();
-        catalog.initialize("rest_catalog", properties);
-
-        String metricsNamespace = properties.get("namespace");
-
-        createOrLoadMetricsTable(catalog, metricsNamespace);
-
+        this.properties = properties;
     }
 
     @Override
     public void report(MetricsReport report) {
+        initializeCatalog();
+
+        String metricsNamespace = properties.getOrDefault(NAMESPACE, NAMESPACE_DEFAULT);
+        createOrLoadMetricsTable(catalog, metricsNamespace, properties.getOrDefault(TABLE_NAME_PREFIX, TABLE_NAME_PREFIX_DEFAULT));
+
+        // TODO: Add the implementation of write metrics to each table
         System.out.println(report);
+
     }
 
-    private void createOrLoadMetricsTable(Catalog catalog, String ns) {
+    private void initializeCatalog() {
+        String catalogName = properties.get(CATALOG_NAME);
+        String catalogImpl = properties.get(CATALOG_IMPL);
+        this.catalog = CatalogUtil.loadCatalog(catalogImpl, catalogName, properties, null);
+    }
+
+    private void createOrLoadMetricsTable(Catalog catalog, String ns, String metricsTableName) {
         Schema scanReportTableSchema = IcebergTableUtil.getCommitReportSchema();
         Schema commitReportTableSchema = IcebergTableUtil.getCommitReportSchema();
 
-        TableIdentifier scanReportTableId = TableIdentifier.of(ns, "report_scan");
-        TableIdentifier commitReportTableId = TableIdentifier.of(ns,  "report_commit");
+        TableIdentifier scanReportTableId = TableIdentifier.of(ns, metricsTableName + "_scan");
+        TableIdentifier commitReportTableId = TableIdentifier.of(ns,  metricsTableName + "_commit");
 
-        IcebergTableUtil.createOrLoadTable(catalog, scanReportTableId, scanReportTableSchema);
-        IcebergTableUtil.createOrLoadTable(catalog, commitReportTableId, commitReportTableSchema);
+        this.scanReportTable = IcebergTableUtil.createOrLoadTable(catalog, scanReportTableId, scanReportTableSchema);
+        this.commitReportTable = IcebergTableUtil.createOrLoadTable(catalog, commitReportTableId, commitReportTableSchema);
     }
-
 }
